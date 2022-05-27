feec0b5690445cfcaac9b699b87a45e6abe89129
Jorge L <jorge.a@coderhouse.com>
2022-05-27 17:40:49 -0500

    call product service from order service

--- apps/order_service/src/order/application/order-creator.ts
+++ apps/order_service/src/order/application/order-creator.ts
@@ -1,11 +1,34 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { HttpStatus, Inject, Injectable, OnModuleInit } from '@nestjs/common'
+import { ClientGrpc } from '@nestjs/microservices'
+import { ProductNotFoundException, ProductOutOfStockException } from './../../../../product_service/src/product/domain'
+import { firstValueFrom } from 'rxjs'
 import { Order, OrderRepository } from '../domain'
+import { FindOneResponse, ProductServiceClient, PRODUCT_SERVICE_NAME } from '../infrastructure/grpc/product.pb'
 
 @Injectable()
-export class OrderCreator {
-  constructor(@Inject('OrderRepository') private readonly orderRepository: OrderRepository) {}
+export class OrderCreator implements OnModuleInit {
+  private productServiceClient: ProductServiceClient
+
+  constructor(
+    @Inject(PRODUCT_SERVICE_NAME) private readonly client: ClientGrpc,
+    @Inject('OrderRepository') private readonly orderRepository: OrderRepository,
+  ) {}
+
+  onModuleInit() {
+    this.productServiceClient = this.client.getService<ProductServiceClient>(PRODUCT_SERVICE_NAME)
+  }
 
   async run(productId: string, quantity: number, userId: string): Promise<string> {
+    const response: FindOneResponse = await firstValueFrom(this.productServiceClient.findById({ id: productId }))
+
+    if (response.status !== HttpStatus.OK) {
+      throw new ProductNotFoundException('Product not found')
+    }
+
+    if (response?.data?.stock <= 0) {
+      throw new ProductOutOfStockException('Product out of stock')
+    }
+
     const order = Order.new(productId, quantity, userId)
 
     await this.orderRepository.create(order)
--- apps/order_service/src/order/order.module.ts
+++ apps/order_service/src/order/order.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common'
+import { ClientsModule, Transport } from '@nestjs/microservices'
+import { join } from 'path'
 import { OrderCreator } from './application'
 import { OrderController, OrderPostgresRepository } from './infrastructure'
+import { PRODUCT_PACKAGE_NAME, PRODUCT_SERVICE_NAME } from './infrastructure/grpc/product.pb'
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: PRODUCT_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../../proto/product.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [OrderController],
   providers: [
     OrderCreator,
